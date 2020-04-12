bbe22e1de0a86b3cad403d0cba74a19ffc10acf6
Artin <lengthmin@gmail.com>
2020-04-12 17:06:25 +0800

    Initial Commit.

---
+++ README.md
@@ -0,0 +1,39 @@
+# mail2rss
+
+低成本的邮件转 RSS 做法。
+使用 [cloudflare workers](https://dash.cloudflare.com/) 和 [testmail.app](https://testmail.app/)。
+
+## 如何使用
+
+将 [mailrss.js](mail2rss.js) 的内容复制到 cloudflare workers 中，填好前面几行的内容即可。
+
+```js
+const allowedTags = ["quartz"]; // 允许请求的 tag
+const testmailNamespace = "xxxxx"; // testmail 的 namespace
+const testmailToken = "xxxxxxxxxxxxxxx"; // testmail 的 api key
+const deployUrl = "https://xxx.xxx.workers.dev/"; // 要部署到的 workers 的域名
+```
+
+deploy 到 workers 之后，你可以用 `{namespace}.{tag}@inbox.testmail.app` 去订阅邮件，然后订阅 `https://xxx.xxx.workers.dev/{tag}` 就可以啦。
+
+假如我的 namespace 是 diyyy，那我就可以用 `diyyy.quartz@inbox.testmail.app` 这个邮箱来订阅 Quartz，然后订阅 `https://xxx.xxx.workers.dev/quartz` 即可。
+
+记得将需要的 tag 添加到第一行 `allowedTags` 里。
+记得将需要的 tag 添加到第一行 `allowedTags` 里。
+记得将需要的 tag 添加到第一行 `allowedTags` 里。
+
+## 注册 testmail.app
+
+testmail 能帮我们接收邮件，免费版每个月可以接收 100 封邮件，邮件可以保存一天。
+
+每个人注册后会获取专属的 namespace，通过 namespace 可以构造不同的邮件地址。
+
+假设我的 namespace 是 `diyyy`，我们可以构造这样的邮件地址 `diyyy.{tag}@inbox.testmail.app`，`{tag}` 可以任意填。
+
+比如，我们可以用 `diyyy.quartz@inbox.testmail.app` 订阅 Quartz 的 newsletter，用 `diyyy.stefanjudis@inbox.testmail.app` 来订阅 Stefan's web dev journey。
+
+testmail 提供了很丰富的 api，获取邮件包括过滤 tag，匹配 tag 前缀，限制获取数量，还支持 GraphQL 查询。
+
+官方文档在这儿：<https://testmail.app/docs/>
+
+注册登录后，在 <https://testmail.app/console> 可以看到自己的 namespace 和 api keys，这两个都是我们需要的。
---
+++ mail2rss.js
@@ -0,0 +1,135 @@
+const allowedTags = ["quartz"];
+const testmailNamespace = "xxxxx";
+const testmailToken = "xxxxxxxxxxxxxxx";
+const deployUrl = "https://xxx.xxx.workers.dev/";
+
+class TestMail {
+  static testmailApi = "https://api.testmail.app/api/graphql";
+
+  static async getMails(tag) {
+    const query = `{
+      inbox (
+        namespace: "${testmailNamespace}"
+        tag: "${tag}"
+        limit: 99
+      ) {
+        emails {
+          id
+          subject
+          html
+          from
+          timestamp
+          downloadUrl
+          attachments {
+            cid
+            downloadUrl
+          }
+        }
+      }
+    }`;
+
+    const init = {
+      method: "POST",
+      headers: {
+        "content-type": "application/json;charset=UTF-8",
+        Authorization: `Bearer ${testmailToken}`,
+        Accept: "application/json",
+      },
+
+      body: JSON.stringify({
+        operationName: null,
+        query,
+        variables: {},
+      }),
+    };
+    return fetch(this.testmailApi, init);
+  }
+}
+
+addEventListener("fetch", (event) => {
+  event.respondWith(handleRequest(event));
+});
+
+/**
+ * Respond to the request
+ * @param {Event} event
+ */
+async function handleRequest(event) {
+  const { request } = event;
+  let url = new URL(request.url);
+  const requestTag = url.pathname.substring(1);
+  if (!allowedTags.includes(requestTag)) {
+    return new Response("Unknown tag.", { status: 403 });
+  }
+
+  let mailResponse = await TestMail.getMails(requestTag);
+  if (mailResponse.status != 200) {
+    return new Response("Internal Server Error.", { status: 500 });
+  }
+  let data = await gatherResponse(mailResponse);
+  let responseXML = await makeRss(data.data.inbox.emails, requestTag);
+  response = new Response(responseXML, {
+    status: 200,
+    headers: {
+      "content-type": "application/xml; charset=utf-8",
+    },
+  });
+  response.headers.append("Cache-Control", "max-age=300");
+
+  return response;
+}
+
+/**
+ * gatherResponse awaits and returns a response body as a string.
+ * Use await gatherResponse(..) in an async function to get the response body
+ * @param {Response} response
+ */
+async function gatherResponse(response) {
+  const { headers } = response;
+  const contentType = headers.get("content-type");
+  if (contentType.includes("application/json")) {
+    return await response.json();
+  } else if (contentType.includes("application/text")) {
+    return await response.text();
+  } else if (contentType.includes("text/html")) {
+    return await response.text();
+  } else {
+    return await response.text();
+  }
+}
+
+async function makeRss(emails, tag) {
+  let items = emails.map((value) => {
+    if (value.attachments.length > 0) {
+      for (let i of value.attachments) {
+        value.html = value.html.replace(`cid:${i.cid}`, i.downloadUrl);
+      }
+    }
+    return `<item>
+    <title><![CDATA[${value.subject}]]></title>
+    <description><![CDATA[${value.html}]]></description>
+    <pubDate>${new Date(value.timestamp).toGMTString()}</pubDate>
+    <guid isPermaLink="false">${value.id}</guid>
+    <link>${value.downloadUrl}</link>
+    <author><![CDATA[${value.from}]]></author>
+</item>`;
+  });
+
+  return `<?xml version="1.0" encoding="UTF-8"?>
+<rss xmlns:atom="http://www.w3.org/2005/Atom" version="2.0">
+    <channel>
+        <title><![CDATA[${tag}邮件订阅]]></title>
+        <link>${deployUrl + tag}</link>
+        <atom:link href="${
+          deployUrl + tag
+        }" rel="self" type="application/rss+xml" />
+        <description><![CDATA[${tag}邮件订阅]]></description>
+        <generator>mail2rss</generator>
+        <webMaster>lengthmin@gmail.com (Artin)</webMaster>
+        <language>zh-cn</language>
+        <lastBuildDate>${new Date().toGMTString()}</lastBuildDate>
+        <ttl>300</ttl>
+        ${items.join("\n")}
+    </channel>
+</rss>`;
+}
